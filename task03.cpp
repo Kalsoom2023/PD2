e1009ab3cb2ca9b21de9bf8a87d20d65f4fd6292
Kalsoom2023 <144407936+Kalsoom2023@users.noreply.github.com>
2023-09-16 18:35:41 +0500

    Add files via upload

---
+++ task03.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"           /\\      "<<endl;
+cout<<"         _/~~\\_     "<<endl;
+cout<<"       /  |  |  \\  "<<endl;
+cout<<"      ====.  .====  "<<endl;
+cout<<"          ||||      "<<endl;
+system("Color 70");
+}
